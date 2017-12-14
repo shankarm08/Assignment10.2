fd06e106c351714e86e80fa921941f77009fbf53
shankarm08 <shankarm08@gmail.com>
2017-12-14 21:23:20 +0530

    updated

---
+++ AssignmentFragg/app/src/main/java/com/wedddingapp/shankar/assignmentfragg/Layout2.java
@@ -0,0 +1,22 @@
+package com.wedddingapp.shankar.assignmentfragg;
+
+/**
+ * Created by shan on 12/12/2017.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Layout2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+// TODO Auto-generated method stub
+        return  inflater.inflate(R.layout.layout2, container, false);
+
+    }
+}
